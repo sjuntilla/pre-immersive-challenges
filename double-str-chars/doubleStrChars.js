aeec4e321cc99faba376547480cd0a34b1ca0fd6
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-08 20:43:20 -1000

    Second commit, starts work on doubleStrChars exercise

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -1,3 +1,11 @@
 module.exports = function doubleStrChars(str) {
   // write your code in here
+  var returnedString = '';
+  if(returnedString === undefined || returnedString === false){
+    return 'not a string!';
+  } else {
+  for (i = 0; i < str.length; i++) {
+    returnedString += str.charAt(i) + str.charAt(i);
+  } return returnedString;
+}
 }
