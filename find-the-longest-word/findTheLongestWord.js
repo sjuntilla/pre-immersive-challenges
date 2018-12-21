1eda584c36a5e9dd2fbc9f75100d3d2349aa2288
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-20 16:34:34 -1000

    Completes 'Find the Longest Word'

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,10 @@
-module.exports = function findTheLongestWord(sen) {
+module.exports = function findTheLongestWord(arr) {
   // write your code in here
+  var longestWord = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > longestWord.length) {
+      longestWord = arr[i];
+    }
+  }
+  return longestWord;
 }
