ef44b4066583349b9cdafa91bdf9cb88131a10da
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-27 17:27:54 -1000

    Finished.

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,4 +1,12 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
-  
+  let arr = [x];
+  let mult = x;
+
+  for (var i = 0; i < length - 1; i++) {
+    mult += x
+    arr.push(mult);
+  }
+  return arr;
+
 }
