fffb7b22cc5c5281a734bdc6ba04cb52a1c62c64
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-27 17:52:32 -1000

    Finished.

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,17 @@
 module.exports = function evenLadderPattern(num) {
   // write your code in here
+
+  if (num <= 1) {
+    return '';
+  } else {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+      if (i % 2 === 0) {
+        const str = i.toString().repeat(i);
+        arr.push(str.toString(''));
+      }
+    }
+    return arr.join('\n');
+  }
+
 }
