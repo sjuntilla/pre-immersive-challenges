2b4c26b2baf23fadb3d2fcb439786a8ff50f097b
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-30 19:29:07 -1000

    Finished.

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,14 @@
 module.exports = function needleInHaystack(haystack) {
   // write code in here
+  /* PSEUDOCODE
+  001: Iterate through 'haystack' to find the fabled 'needle', and return its index.
+  002. If 'needle' doesn't exist, return 'false.' */
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack.indexOf('needle') > -1) {
+      return haystack.indexOf('needle');
+    } else {
+      return false;
+    }
+  }
 }
