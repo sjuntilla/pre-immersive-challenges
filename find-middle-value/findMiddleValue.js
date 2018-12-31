edf466fbb20861b560b29168eaec45ff48c8c49c
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-30 17:47:05 -1000

    Finished.

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,3 +1,13 @@
 module.exports = function findMiddleValue(numArray) {
   // write your code in here
+  let arr = numArray.slice();
+  let arrSort = numArray.sort((a, b) => {
+    return a - b;
+  });
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === arrSort[1]) {
+      return arr.indexOf(arr[i]);
+    }
+  }
 }
