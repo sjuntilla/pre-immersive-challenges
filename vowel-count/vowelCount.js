4e66c128ec20328a38960d40ea3ee1b762716127
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-30 19:38:42 -1000

    Finished.

--- vowel-count/vowelCount.js
+++ vowel-count/vowelCount.js
@@ -1,3 +1,14 @@
 module.exports = function vowelCount(str) {
   // write your code in here
+  /* PSEUDOCODE
+  001. Iterate through 'str' as an array to determine which letters are vowels or not.
+  002. Add to the vowel count every time a vowel is detected. */
+  let arr = str.split('');
+  let vowelCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].match(/[aeiou]/)) {
+      vowelCount++;
+    }
+  }
+  return vowelCount;
 }
