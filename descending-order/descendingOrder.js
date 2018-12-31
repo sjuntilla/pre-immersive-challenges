2f1c2f1691a3418b5601a5a3b525e169d9ea962c
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-30 15:33:46 -1000

    Finished.

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,11 @@
 module.exports = function descendingOrder(number) {
   // write code in here
+  if (typeof number === 'number') {
+    let num = number.toString();
+    let arr = Array.from(num).map(Number);
+    return arr.sort().reverse().join('');
+
+  } else {
+    return 'not a number!';
+  }
 }
