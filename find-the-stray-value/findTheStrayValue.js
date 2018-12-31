0b92ab71d0ade99d991bc36e26c6586d9125e891
Samantha Juntilla <juntilla@hawaii.edu>
2018-12-30 17:54:24 -1000

    Finished.

--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,10 @@
 module.exports = function findTheStrayValue(array) {
   // write your code in here
+  let stray = array[0];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== array[0]) {
+      stray = array[i];
+    }
+  }
+  return stray;
 }
--- half-life/halfLife.js
+++ half-life/halfLife.js
@@ -1,3 +1,7 @@
 module.exports = function halfLife(initialQuantity, quantityRemaining, time) {
-  
+    const halfs = 0;
+    const halved = initialQuantity / 2;
+    while (quantityRemaining > halved) {
+        halved++;
+    }
 }
